6c90880f2845c57c18f415033c7f964557a00aa0
Byongho <unlike96@gmail.com>
2024-07-17 19:11:10 +0900

    upload wasm/pkg

---
+++ simple/rust-wasm-array-context/wasm-array/pkg/wasm_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_ball_free(a: number): void;
+export function __wbg_balls_free(a: number): void;
+export function ball_new(a: number, b: number, c: number, d: number): number;
+export function ball_update(a: number, b: number, c: number): void;
+export function ball_x(a: number): number;
+export function ball_y(a: number): number;
+export function balls_new(a: number, b: number, c: number, d: number): number;
+export function balls_update(a: number): void;
+export function balls_length(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_exn_store(a: number): void;
